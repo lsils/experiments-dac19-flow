6cdcd1876d61d9bb8f992095f8614bd8c4eda02a
Heinz Riener <heinz.riener@gmail.com>
2019-04-04 14:36:24 +0200

    execution script.

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import json
 import cirkit
+import subprocess
+
+### Global settings
+verbose = False
+print_progress = False
 
 ### Misc
 class color:
@@ -19,23 +24,23 @@ benchmarks = {
   "adder":      { 'verify': True },
   "arbiter":    { 'verify': True },
   "bar":        { 'verify': True },
-  "cavlc":      { 'verify': True },
-  "ctrl":       { 'verify': True },
-  "dec":        { 'verify': True },
-  "div":        { 'verify': True },
-  "i2c":        { 'verify': True },
-  "int2float":  { 'verify': True },
-  "log2":       { 'verify': True },
-  "max":        { 'verify': True },
-  "mem_ctrl":   { 'verify': True },
-  "multiplier": { 'verify': True },
-  "priority":   { 'verify': True },
-  "router":     { 'verify': True },
-  "sin":        { 'verify': True },
-  "sqrt":       { 'verify': True },
-  "square":     { 'verify': True },
-  "voter":      { 'verify': True },
-  "hyp":        { 'verify': False },
+#  "cavlc":      { 'verify': True },
+#  "ctrl":       { 'verify': True },
+#  "dec":        { 'verify': True },
+#  "div":        { 'verify': True },
+#  "i2c":        { 'verify': True },
+#  "int2float":  { 'verify': True },
+#  "log2":       { 'verify': True },
+#  "max":        { 'verify': True },
+#  "mem_ctrl":   { 'verify': True },
+#  "multiplier": { 'verify': True },
+#  "priority":   { 'verify': True },
+#  "router":     { 'verify': True },
+#  "sin":        { 'verify': True },
+#  "sqrt":       { 'verify': True },
+#  "square":     { 'verify': True },
+#  "voter":      { 'verify': True },
+#  "hyp":        { 'verify': False },
 }
 
 ### Configurations
@@ -46,40 +51,44 @@ configurations = {
     "xmg" : {}
 }
 
+### Cirkit wrapper calls
 def aigerfile(name):
-    return f"benchmarks/{name}.aig"
+   return f"benchmarks/{name}.aig"
 
 def resultfile(name, suffix, ext):
-    return f"results/{name}_{suffix}.{ext}"
+   return f"results/{name}_{suffix}.{ext}"
 
 def read(name,filename):
-    return cirkit.read_aiger(filename=filename, **{name : True})
+   return cirkit.read_aiger(filename=filename, **{name : True})
+
+def write(name,filename):
+   cirkit.write_verilog(filename=filename, **{name : True})
 
 def clear_store():
-    cirkit.store(clear=True, aig=True, mig=True, xag=True, xmg=True, lut=True)
+   cirkit.store(clear=True, aig=True, mig=True, xag=True, xmg=True, lut=True)
 
 def ps(name):
-  return cirkit.ps(silent=True, **{name : True})
+   return cirkit.ps(silent=True, **{name : True})
 
 def lut_mapping(name):
-  return cirkit.lut_mapping(**{name : True})
+   return cirkit.lut_mapping(**{name : True})
 
 def collapse_mapping(name):
-  return cirkit.collapse_mapping(**{name : True})
+   return cirkit.collapse_mapping(**{name : True})
 
-def compute_stats(name,filename):
-    ntk_stats = ps(name).dict()
-    lut_mapping(name)
-    collapse_mapping(name)
-    lut_stats = ps('lut').dict()
+def compute_stats(name):
+   ntk_stats = ps(name).dict()
+   lut_mapping(name)
+   collapse_mapping(name)
+   lut_stats = ps('lut').dict()
 
-    statistics = {
-        'depth': ntk_stats['depth'],
-        'gates' : ntk_stats['gates'],
-        'luts' : lut_stats['gates']
-    }
+   statistics = {
+      'depth': ntk_stats['depth'],
+      'gates' : ntk_stats['gates'],
+      'luts' : lut_stats['gates']
+   }
 
-    return statistics
+   return statistics
 
 def rfz(name):
    if ( name == 'mig' ):
@@ -131,6 +140,7 @@ def bz(name):
    else:
       return {'time_total': 0.0}
 
+### Optimization script
 compress2rs = [
     [bz, {}],
     [rs, {'cut_size': 6}],
@@ -185,46 +195,66 @@ table = {}
 # }
 
 for benchmark, benchmark_params in benchmarks.items():
-    for name, configuration in configurations.items():
-        print(f"[i] run {benchmark} with {name}")
+   for name, configuration in configurations.items():
+      print(f"[i] run {benchmark} with {name}")
 
-        # read benchmark
-        in_filename = aigerfile(benchmark)
-        read(name, in_filename)
+      # read benchmark
+      in_filename = aigerfile(benchmark)
+      read(name, in_filename)
 
-        # compute statistics for initial benchmark
-        stats_before = compute_stats(name, in_filename)
+      # compute statistics for initial benchmark
+      stats_before = compute_stats(name)
 
-        # clear storage and re-read benchmark
-        clear_store()
-        read(name, in_filename)
+      # clear storage and re-read benchmark
+      clear_store()
+      read(name, in_filename)
 
-        # run flow script
-        stats_opt = run_flow(compress2rs)
+      # run flow script
+      stats_opt = run_flow(compress2rs, verbose)
 
-        # compute statistics for optimized benchmark
-        stats_after = compute_stats(name, in_filename)
+      # compute statistics for optimized benchmark
+      stats_after = compute_stats(name)
 
-        # update table
-        if (not benchmark in table):
-            table[benchmark] = {
-                'baseline': {
-                    'gates': stats_before['gates'],
-                    'depth': stats_before['depth'],
-                    'luts': stats_before['luts'],
-                    'time': 0.0,
-                },
-                name: {
-                    'gates': stats_after['gates'],
-                    'depth': stats_after['depth'],
-                    'luts': stats_after['luts'],
-                    'time': stats_opt['time_total'],
-                }
+      # write result file
+      out_filename = resultfile(benchmark, name, 'v')
+      write(name, out_filename)
+
+      # verify final result using ABC CEC
+      verified = color.BLUE + '[not checked]' + color.ENDC
+      if benchmark_params['verify']:
+         subprocess.call([ 'abc -c \"cec -n %s %s\" &> abc.log' % (in_filename, out_filename) ], shell=True )
+         with open('abc.log') as f:
+            lines = f.readlines()
+            if lines[2][:23] == "Networks are equivalent":
+               verified = color.GREEN + '[verified]' + color.ENDC
+            else:
+               print('[e] verification after optimization failed')
+               verified = color.RED + '[failed]' + color.ENDC
+
+      # update table
+      if (not benchmark in table):
+         table[benchmark] = {
+            'baseline': {
+               'gates': stats_before['gates'],
+               'depth': stats_before['depth'],
+               'luts': stats_before['luts'],
+               'time': 0.0,
+            },
+            name: {
+               'gates': stats_after['gates'],
+               'depth': stats_after['depth'],
+               'luts': stats_after['luts'],
+               'time': stats_opt['time_total'],
             }
-        else:
-            table[benchmark][name] = {
-                'gates': stats_after['gates'],
-                'depth': stats_after['depth'],
-                'luts': stats_after['luts'],
-                'time': stats_opt['time_total'],
-            }
+         }
+      else:
+         table[benchmark][name] = {
+            'gates': stats_after['gates'],
+            'depth': stats_after['depth'],
+            'luts': stats_after['luts'],
+            'time': stats_opt['time_total'],
+         }
+
+      # print progress for each benchmark
+      if print_progress:
+         print(table[benchmark][name], verified)
